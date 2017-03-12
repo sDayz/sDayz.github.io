4aa07626755bf9d146999284db91301e6784a984
Philip L <Philip L>
2017-03-12 06:32:37 -0700

    adding jquery and loading placeholder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (function() {
-
-document.querySelector("#main").style.display = "block";
-document.querySelector("#loader-wrapper").style.display = "none";
-
+setTimeout(function () {
+  document.querySelector("#main").style.display = "block";
+  document.querySelector("#loader-wrapper").style.display = "none";
+}, 200); // Placeholder to show loading screen
 
 })();
